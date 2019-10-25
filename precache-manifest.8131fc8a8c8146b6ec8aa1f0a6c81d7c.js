f21c197485682f2d58c0338f4ce46ec73e1c090f
Theethawat Savastham <5910110150@psu.ac.th>
2019-10-26 01:53:59 +0700

    Updates

---
+++ precache-manifest.8131fc8a8c8146b6ec8aa1f0a6c81d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cf4ec00491e924b71574de1037ad11",
+    "url": "/dentcongrat/index.html"
+  },
+  {
+    "revision": "71802c3c3264280a437c",
+    "url": "/dentcongrat/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7db1378e9ebfd37731f4",
+    "url": "/dentcongrat/static/css/main.045f45d9.chunk.css"
+  },
+  {
+    "revision": "71802c3c3264280a437c",
+    "url": "/dentcongrat/static/js/2.ecd28816.chunk.js"
+  },
+  {
+    "revision": "7db1378e9ebfd37731f4",
+    "url": "/dentcongrat/static/js/main.cf6ed67f.chunk.js"
+  },
+  {
+    "revision": "7899432e2f13eee3916f",
+    "url": "/dentcongrat/static/js/runtime-main.0e73c3ae.js"
+  }
+]);
